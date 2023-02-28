c912b14da3b1f74044c4c3800a5f7316fb2a7bae
Lino Puma Ticona <nearlino20@gmail.com>
2023-02-28 10:36:50 -0500

    Servicios y  nosotros

--- pages/Nosotros/index.vue
+++ pages/Nosotros/index.vue
@@ -1,5 +1,51 @@
 <template>
   <div class="page-nosotros">
     <HeaderPages titulo="Nosotros"></HeaderPages>
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-8">
+          <SectionsTitulo
+            titulo="GRUPO DREWS"
+            descripcion=" Alquiler de camionetas 4x4 y otros veiculos motorizados"
+          ></SectionsTitulo>
+          <p>
+            Grupo Drews forma parte de un grupo empresarial conformado por
+            Servicios Múltiples Drews S.A.C. – (Drews Rent A Car), Servicios
+            Generales Drews Perú S.A.C., Esbril S.A.C., Intermotors S.A.C. y
+            Tamawi S.A.C. con amplia experiencia en el sector Automotriz. Somos
+            un grupo empresarial con más de 10 años de experiencia en la Región
+            Puno y el Sur del Perú con el objetivo de brindar servicios de
+            alquiler de Camionetas 4X4, Alquiler de maquinaria pesada y otros
+            Vehículos motorizados. Servicio de taller mecánico, Auxilio
+            mecánico, Venta de repuestos, Asesoría y venta de seguros,
+            Consultorías, Movimiento de tierras, Líneas de media tensión,
+            Ejecución de obras y otros.
+          </p>
+          <p>
+            Drews Rent A Car, inicia sus labores en la región puno en el año
+            2007, con el objetivo de brindar servicio de camionetas 4×4, en los
+            diferentes sectores y actividades de desarrollo en la región,
+            centrándonos con mayor interés en los sectores de minería,
+            exploraciones, energía, construcción, ingeniería, etc.
+          </p>
+        </div>
+        <div class="col-4 h100 bg-secondary d-flex flex-wrap">
+          <div class="w-100 text-end mt-5">
+            <h1 class="text-color1">10 +</h1>
+            <h5>Experiencia</h5>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <img
+              class="rounded-4"
+                width="100%"
+                src="~/assets/images/demos/vehiculos/car-g78c70543c_640.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- pages/Servicios/index.vue
+++ pages/Servicios/index.vue
@@ -1,5 +1,82 @@
 <template>
   <div class="page-servicios">
     <HeaderPages titulo="Servicios"></HeaderPages>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <SectionsTitulo titulo="Servicios" descripcion="Nuestros Servicios" />
+        </div>
+      </div>
+      <div class="row mb-5 gy-3">
+
+          <div class="col-4" v-for="item in 8" :key="item">
+            <div
+              class="card card-item bg-white w-100 border-white shadow"
+              data-aos="fade-up"
+            >
+              <div class="card-body">
+                <div class="icono-wrap">
+                  <Icon
+                    class="icono"
+                    name="ic:twotone-business-center"
+                    size="50px"
+                  />
+                </div>
+                <h4 class="card-title w-100 text-center my-4">CORPORATIVO</h4>
+                <p class="mb-0 text-center w-100 px-2 pb-2">
+                  Contamos con una variedad de modelos de Camionetas 4x4, 4x2,
+                  Vans, Minivans y Custers, con la implementación y equipamiento
+                  requerido. Tarifas corporativas.
+                </p>
+              </div>
+            </div>
+          </div>
+
+       
+        </div>
+    
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.page-servicios {
+  .card.card-item .card-body {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    .icono-wrap {
+      position: relative;
+      padding: 5px;
+
+      .icono {
+        z-index: 5;
+        position: relative;
+        color: #0c7f40;
+      }
+
+      &::after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 10px;
+        height: 10px;
+        clip-path: circle(50% at 50% 50%);
+        background-color: #eee;
+        z-index: 0;
+
+        transition: ease-in-out width 0.5s, height 0.5s;
+      }
+
+      &:hover {
+        &::after {
+          width: 50px;
+          height: 50px;
+        }
+      }
+    }
+  }
+}
+</style>
